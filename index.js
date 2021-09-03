4dd6748f1572468a761de57f64cd8c53d610ff45
Lyka <66162537+lykaoclarit@users.noreply.github.com>
2021-09-03 12:53:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+//swapping variables
+function test() {
+    var a = "3";
+    var b = "8";
+  
+//sol1 - make a third var
+    var c =a;
+  a = b;
+  b = c;
+
+//sol2  
+b=[a, a=b][0];
+  
+    console.log("a is " + a);
+    console.log("b is " + b);
+}
+
 //char length
 var tweet=prompt("What's on your mind?");
 alert("The length of your tweet is " + tweet.length + ".");
@@ -16,7 +33,7 @@ allChar = allChar.slice(1,name.length);
 allName = firstChar + allChar;
 alert("Hello there, " + allName + "!");
 
-//buy a number of bottle of drinks base on the price
+//buy a number of bottle of drinks base on the price per bottke
 function getDrink(money) {   
 var bottleCost = money / 5;
   console.log("buy " + bottleCost);
